cb157fb42aeb8b901ecf4804fb6e7439ad69363b
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2022-08-31 18:18:29 +0200

    Check for /etc/bash.bashrc vs /etc/bashrc
    - Added support for Fedora like systems (/etc/bashrc)

--- install_dotnet60.sh
+++ install_dotnet60.sh
@@ -15,10 +15,23 @@ cd /.netRuntime/dotnet60
 curl -L -o dotnet-runtime-6.0.8-linux-x64.tar.gz "https://download.visualstudio.microsoft.com/download/pr/5596ef6f-a174-4fba-aef1-99173e3f3c67/77edd755d605688885ca7114bc4f9ae9/dotnet-runtime-6.0.8-linux-x64.tar.gz"
 tar zxf dotnet-runtime-6.0.8-linux-x64.tar.gz -C /.netRuntime/dotnet60
 
-# Users
-echo export DOTNET_ROOT=/.netRuntime/dotnet60 >> /etc/bash.bashrc
-echo export PATH=\$PATH:/.netRuntime/dotnet60 >> /etc/bash.bashrc
+# Set user Environment variables
 
-# Root
+# Ubuntu
+FILE=/etc/bash.bashrc
+if test -f "$FILE"; then
+    echo export DOTNET_ROOT=/.netRuntime/dotnet60 >> /etc/bash.bashrc
+    echo export PATH=\$PATH:/.netRuntime/dotnet60 >> /etc/bash.bashrc
+fi
+
+# Fedora
+FILE=/etc/bashrc 
+if test -f "$FILE"; then
+    echo export DOTNET_ROOT=/.netRuntime/dotnet60 >> /etc/bashrc
+    echo export PATH=\$PATH:/.netRuntime/dotnet60 >> /etc/bashrc
+fi
+
+# Set Root Environment variables
 echo export DOTNET_ROOT=/.netRuntime/dotnet60 >> /etc/skel/.bashrc
 echo export PATH=\$PATH:/.netRuntime/dotnet60 >> /etc/skel/.bashrc
+
