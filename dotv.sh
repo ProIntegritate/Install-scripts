6bebf5a7dcb488b90d3e153dead6db48bb209b75
ProIntegritate <54767353+ProIntegritate@users.noreply.github.com>
2023-11-15 22:08:11 +0100

    Update dotv.sh

--- dotv.sh
+++ dotv.sh
@@ -6,50 +6,39 @@
 if [ -z "$1" ]
   then
     echo "Usage examples:"
-    echo "sudo ./dotv.sh 80 = will set paths to /.netRuntime/dotnet80"
-    echo "sudo ./dotv.sh 60 = will set paths to /.netRuntime/dotnet60"
+    echo "sudo dotv.sh 80 = will set paths to /.netRuntime/dotnet80"
+    echo "sudo dotv.sh 60 = will set paths to /.netRuntime/dotnet60"
     exit 0
 fi
 
 
 # Set user Environment variables for dotnet
 DOTNET=dotnet$1
-echo "Setting dotnet version: $DOTNET"
+echo "* Setting dotnet version: $DOTNET"
+
+
+function processfile() {
+	if test -f "$FILE"; then
+		echo "* Modifying $FILE"
+		cp $FILE $FILE.bak
+		cat $FILE | grep -v "netRuntime" > $FILE.new
+		mv $FILE.new $FILE
+		echo export DOTNET_ROOT=/.netRuntime/$DOTNET >> $FILE
+		echo export PATH=\$PATH:/.netRuntime/$DOTNET >> $FILE
+	fi
+}
 
 
 # Ubuntu flavour
 FILE=/etc/bash.bashrc
-if test -f "$FILE"; then
-	echo Modifying $FILE
-	cp $FILE $FILE.bak
-	cat $FILE | grep -v "netRuntime" > $FILE.new
-	mv $FILE.new $FILE
-	echo export DOTNET_ROOT=/.netRuntime/$DOTNET >> $FILE
-	echo export PATH=\$PATH:/.netRuntime/$DOTNET >> $FILE
-fi
-
+processfile
 
 # Fedora/Redhat flavour
-FILE=/etc/bashrc 
-if test -f "$FILE"; then
-	echo Modifying $FILE
-	cp $FILE $FILE.bak
-	cat $FILE | grep -v "netRuntime" > $FILE.new
-	mv $FILE.new $FILE
-	echo export DOTNET_ROOT=/.netRuntime/$DOTNET >> $FILE
-	echo export PATH=\$PATH:/.netRuntime/$DOTNET >> $FILE
-fi
-
+FILE=/etc/bashrc
+processfile
 
 # Set Root Environment variables
 FILE=/etc/skel/.bashrc
-if test -f "$FILE"; then
-	echo Modifying $FILE
-	cp $FILE $FILE.bak
-	cat $FILE | grep -v "netRuntime" > $FILE.new
-	mv $FILE.new $FILE
-	echo export DOTNET_ROOT=/.netRuntime/$DOTNET >> $FILE
-	echo export PATH=\$PATH:/.netRuntime/$DOTNET >> $FILE
-fi
+processfile
 
-echo "Done. Please restart any terminal/shell to update your environmental variables."
+echo "Done! Please restart any terminal/shell to update your environmental variables."
